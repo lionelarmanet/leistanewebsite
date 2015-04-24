3e8ce857d6847cfb789e525d81d9bcb9a5035e8e
Julien Reboul <julien.reboul@bonitasoft.com>
2015-04-24 22:51:59 +0200

    add gulpfile but uglify does not push to dist/scripts

--- main/scripts/nav.js
+++ main/scripts/nav.js
@@ -1,6 +1,6 @@
 /* globals $ */
 (function() {
-  'use strict'; 
+  'use strict';
   $(function() {
 
     $('nav a').click( function(e) {
@@ -15,11 +15,11 @@
     });
 
   });
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 })();
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
